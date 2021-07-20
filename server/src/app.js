c11c87bc43470dd5602287d6e32d0a600324c69b
sevalkurtulus <kurtulusseval06@hotmail.com>
2021-07-20 23:40:40 +0300

    setup of the express js server

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,20 @@
 console.log("hello");
+
+const express = require("express");
+const bodyParser = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+
+const app = express();
+
+app.use(morgan("combine"));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get("/status", (req, res) => {
+  res.send({
+    message: "hello world!",
+  });
+});
+
+app.listen(process.env.PORT || 8081);
